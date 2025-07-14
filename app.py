657718035193dcfdec58b6fb8c6b84ae7f070412
aud-g <bansheeaud@gmail.com>
2025-07-14 06:17:47 +0000

    update

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ def prediction():
     # make prediction
     pred = model.predict([[q]])
 
-    return(render_template("prediction.html",r=pred)
-
+    return(render_template("prediction.html",r=pred))
+    
 if __name__ == "__main__":
     app.run()
     
