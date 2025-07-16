867da311d0ee7801876b9f25a676364c7622eecb
aud-g <bansheeaud@gmail.com>
2025-07-16 12:00:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import joblib
 from groq import Groq
-import requests
 
 import os
 os.environ['GROQ_API_KEY'] = os.getenv('groq')
@@ -81,9 +80,23 @@ def prediction():
 
 @app.route("/telegram",methods=["GET","POST"])
 def telegram():
+    domain_url = "https://dbss-1-ai0s.onrender.com"
+
+    # The following line is used to delete the existing webhook URL for the Telegram bot
+    delete_workbook_url = "http://api.telegram.org/bot{7456173671:AAGJDt2dGNyn9xTGWrQDzxPL84jp4IBzvSs}/deleteWebhook"
+    requests.post(delete_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+
+    # Set the webhook URL for the Telegram bot
+    set_webhook_url = f"https://api.telegram.org/bot{7456173671:AAGJDt2dGNyn9xTGWrQDzxPL84jp4IBzvSs}/setWebhook?url={domain_url}/webhook"
+    webhook_response = requests.post(set_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+
+    if webhook_response.status_code == 200:
+        # set status message
+        status = "The telegram bot is running. Please check with the telegram bot. @your_bot"
+    else:
+        status = "Failed to start the telegram bot. Please check the logs."
     
-    return(render_template("telegram.html"))
+    return(render_template("telegram.html", status=status))
 
 if __name__ == "__main__":
     app.run()
-    
